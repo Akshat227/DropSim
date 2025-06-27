0684a4e47bf9f22756741d497a1d432811abcd5b
akshat227 <akshatkhare364@gmail.com>
2025-06-27 15:29:59 +0530

    boxes and gravity added

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
+#include <vector>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+int GrainW = 10;
+int GrainH = 10;
+
+struct FallingRect
+{
+    SDL_Rect rect;
+    float V_velocity; // vertical velocity
+};
 
 int main(int argc, char *args[])
 {
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("DropSIM",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        650, 600, SDL_WINDOW_SHOWN);
-        
-        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-        
-        
-        
-       
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          650, 600, SDL_WINDOW_SHOWN);
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-     bool running = true;
+    std::vector<FallingRect> fallingRects;
+    const float gravity = 0.5f;
+
+    bool running = true;
     SDL_Event event;
     while (running)
     {
@@ -27,37 +34,48 @@ int main(int argc, char *args[])
             if (event.type == SDL_QUIT)
                 running = false;
 
-            if (event.type == SDL_MOUSEMOTION) {
-            std::cout << "Mouse Motion Detected - "
-            << "x: " << event.motion.x
-            << ", y: " << event.motion.y << '\n';
-            }
+            if (event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                if (event.button.button == SDL_BUTTON_LEFT)
+                {
+                    FallingRect Fr;
+                    Fr.rect = {event.button.x, event.button.y, GrainW, GrainH};
+                    Fr.V_velocity = 0;
+                    fallingRects.push_back(Fr);
+                }
 
-            if (event.type == SDL_MOUSEBUTTONDOWN){
-            if (event.button.button == SDL_BUTTON_LEFT)
-            {
-            std::cout << "left mouse button pressed" << std::endl;
-            } 
-            else if (event.button.button == SDL_BUTTON_RIGHT) 
-            {
-            std::cout << "Right mouse button pressed" << std::endl;
             }
-            }
-            
-           
+                for (auto &r : fallingRects)
+                {
+                    r.V_velocity += gravity;
+                    r.rect.y += static_cast<int>(r.V_velocity);
+
+                    // Stop at the floor
+                    if (r.rect.y + r.rect.h > 600)
+                    {
+                        r.rect.y = 600 - r.rect.h;
+                        r.V_velocity = 0; // Stop falling
+                    }
+                }
         }
-        
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        
+
         SDL_RenderClear(renderer);
-        
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        for (const auto &r : fallingRects)
+        {
+            SDL_RenderFillRect(renderer, &r.rect);
+        }
+
         SDL_RenderPresent(renderer);
-        
+        SDL_Delay(16); // ~60 FPS
     }
-        
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
